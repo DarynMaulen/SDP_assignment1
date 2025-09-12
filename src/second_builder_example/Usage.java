d8f3ba404e37775dcc594c2e0c8d3d76175f2640
Дарын <241245@astanait.edu.kz>
2025-09-12 10:19:05 +0300

    fix

--- src/second_builder_example/Usage.java
+++ src/second_builder_example/Usage.java
@@ -1,12 +1,19 @@
 package second_builder_example;
 
+import first_builder_example.CarBuilder;
+import first_builder_example.CarDirector;
+import first_builder_example.SportsCarBuilder;
+
 public class Usage {
-    Car sportsCar = new Car.Builder()
-            .setManufacturer("Ferrari")
-            .setModel("488 GTB")
-            .setYear(2022)
-            .setBodyType("Coupe")
-            .setEngine("V8 Twin Turbo")
-            .setColor("Red")
-            .build();
+    public static void main(String[] args) {
+        Car sportsCar = new Car.Builder()
+                .setManufacturer("Ferrari")
+                .setModel("488 GTB")
+                .setYear(2022)
+                .setBodyType("Coupe")
+                .setEngine("V8 Twin Turbo")
+                .setColor("Red")
+                .build();
+        System.out.println(sportsCar);
+    }
 }
